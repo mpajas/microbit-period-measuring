94c8d4443fee6b12f079378a72c6e0708560aa2b
Matija Pajas <23265032+mpajas@users.noreply.github.com>
2020-05-31 21:45:43 +0200

    improved readability and optimized N and time_step

--- spring.py
+++ spring.py
@@ -1,43 +1,55 @@
 from microbit import *
 from array import array
+
+N = 300
+time_step = 30
+
 def mean(arr):
     return sum(arr)/len(arr)
+
 def autocorrelate(data):
     autocorrel = array("f",[0]*len(data))
     for i in range(len(data)):
         for j in range(len(data)):
             autocorrel[i] += data[j] * data[ ( j + i ) % len(data)]
     return autocorrel
+
 def find_peaks(data):
     peaks = []
     for i in range(1, len(data)-1):
         if(data[i-1]<data[i] and data[i+1]<data[i]):
             peaks.append(i)
     return peaks
+
 def calculate_period(peaks):
     diff = 0
     for i in range(len(peaks)-1):
         diff+=peaks[i+1]-peaks[i]
-    return diff/(len(peaks)-1)*40*1e-3
-data = []
-display.show("3")
-sleep(1000)
-display.show("2")
-sleep(1000)
-display.show("1")
-sleep(1000)
-data = array("f",[0]*350)
-for i in range(350):
-    if i%25==0:
-        display.scroll(str((350-i)//25),delay=50)
-    sleep(40)
+    return diff/(len(peaks)-1)*time_step*1e-3
+
+for i in range(3):
+    display.show(str(3-i))
+    sleep(1000)
+
+data = array("f",[0]*N)
+display.show("M",delay=50)
+
+for i in range(N):
+    sleep(time_step)
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     z = accelerometer.get_z()
     norm = (x**2 + y**2 + z**2)**(1/2)
     data[i]= norm
+
 display.scroll("Calculating...")
-T = calculate_period(find_peaks(autocorrelate(data)))
+
+autocor = autocorrelate(data)
+peaks = find_peaks(autocor)
+T = calculate_period(peaks)
+pi = 3.14159265359
+g = 9.81
+
 while True:
     display.show("A")
     if button_a.is_pressed():
